9be9dd0e151e7e641605a023074157a6fe914791
Romanyerom <romanyerom@github.com>
2023-03-19 12:57:35 -0700

    UNIX is basically a simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const int len = sizeof(msg) - 1;
+	write(2, msg, len);
+	return 1;
+}
