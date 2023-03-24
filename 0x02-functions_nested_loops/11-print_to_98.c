9ff5aa7319e42d1ee1d600fecbed2fc33e14cd6d
Romanyerom <romanyerom@github.com>
2023-03-24 01:06:56 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * Print_to_98 - Prints all natural numbers from n to 98
+ *
+ * @n: The starting number
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	/* loop from n to 98 */
+	while (n != 98)
+	{
+		/* print the current number */
+		printf("%d, ", n);
+
+		/* increment or decrement n based on its value relative to 98 */
+		if (n < 98)
+			n++;
+		else
+			n--;
+	}
+
+	/* print the final number, 98 */
+	printf("98\n");
+}
