7134e802caf90f4f441bdc421d538a2e1d7035cc
Romanyerom <romanyerom@github.com>
2023-03-27 04:09:13 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
- *
- * Return: void
+ * print_numbers - prints the numbers since 0 up to 9
+ * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
