7e20d6e7fe34307a71980d32149913c0b7262886
Romanyerom <romanyerom@github.com>
2023-03-27 08:00:41 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,13 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Prints the numbers from 1 to 100, with Fizz for multiples of
- * three,Buzz for multiples of five, and FizzBuzz for multiples of both three
- * and five.
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
@@ -17,23 +12,14 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz ");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz ");
-		}
 		else
-		{
 			printf("%d ", i);
-		}
 	}
-
 	printf("\n");
 
 	return (0);
