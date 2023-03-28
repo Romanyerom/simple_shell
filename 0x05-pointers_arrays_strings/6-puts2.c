3381ed7d8a5cfa618ae98efdfbecbb5d12762d8f
Romanyerom <romanyerom@github.com>
2023-03-28 13:47:44 -0700

    Half the lies

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string.
- * @str: The string to be printed.
+ * puts2 - Print strings
+ * @str: function parameter
  */
 void puts2(char *str)
 {
 	int i;
 
-	/* Iterate through the string, incrementing by 2 each time */
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
 		_putchar(str[i]);
+		i++;
 	}
 
-	/* Print a new line at the end */
 	_putchar('\n');
 }
